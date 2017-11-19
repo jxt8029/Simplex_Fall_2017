acbb5c628365fc8461d698e70caea8de7e4a3b19
Jake <jxt8029@rit.edu>
2017-11-19 15:29:41 -0500

    E08 - EntityManager completed with one small collision issue on initialization which is fixed once the models translate, details in README.

--- E08 - EntityManager/MyEntityManager.cpp
+++ E08 - EntityManager/MyEntityManager.cpp
@@ -4,9 +4,16 @@ using namespace Simplex;
 MyEntityManager* MyEntityManager::m_pInstance = nullptr;
 void MyEntityManager::Init(void)
 {
+	m_entityList.clear();
+	m_uEntityCount = 0;
 }
 void MyEntityManager::Release(void)
 {
+	for(int i = 0; i < m_uEntityCount; i++)
+	{
+		SafeDelete(m_entityList[i]);
+	}
+	Init();
 }
 MyEntityManager* MyEntityManager::GetInstance()
 {
@@ -18,43 +25,62 @@ MyEntityManager* MyEntityManager::GetInstance()
 }
 void MyEntityManager::ReleaseInstance()
 {
+	if (m_pInstance != nullptr)
+	{
+		delete m_pInstance;
+		m_pInstance = nullptr;
+	}
 }
 int Simplex::MyEntityManager::GetEntityIndex(String a_sUniqueID)
 {
-	return -1;
+	for(int i = 0; i < m_uEntityCount; i++)
+	{
+		if (m_entityList[i]->GetUniqueID() == a_sUniqueID) 
+		{
+			return i;
+		}
+	}
 }
 //Accessors
 Model* Simplex::MyEntityManager::GetModel(uint a_uIndex)
 {
-	return nullptr;
+	return m_entityList[a_uIndex]->GetModel();
 }
 Model* Simplex::MyEntityManager::GetModel(String a_sUniqueID)
 {
-	return nullptr;
+	return MyEntity::GetEntity(a_sUniqueID)->GetModel();
 }
 RigidBody* Simplex::MyEntityManager::GetRigidBody(uint a_uIndex)
 {
-	return nullptr;
+	return m_entityList[a_uIndex]->GetRigidBody();
 }
 RigidBody* Simplex::MyEntityManager::GetRigidBody(String a_sUniqueID)
 {
-	return nullptr;
+	return MyEntity::GetEntity(a_sUniqueID)->GetRigidBody();
 }
 matrix4 Simplex::MyEntityManager::GetModelMatrix(uint a_uIndex)
 {
-	return IDENTITY_M4;
+	if (a_uIndex >= m_uEntityCount)
+	{
+		a_uIndex = m_uEntityCount - 1;
+	}
+	return m_entityList[a_uIndex]->GetModelMatrix();
 }
 matrix4 Simplex::MyEntityManager::GetModelMatrix(String a_sUniqueID)
 {
-	return IDENTITY_M4;
+	return MyEntity::GetEntity(a_sUniqueID)->GetModelMatrix();
 }
 void Simplex::MyEntityManager::SetModelMatrix(matrix4 a_m4ToWorld, String a_sUniqueID)
 {
-
+	MyEntity::GetEntity(a_sUniqueID)->SetModelMatrix(a_m4ToWorld);
 }
 void Simplex::MyEntityManager::SetModelMatrix(matrix4 a_m4ToWorld, uint a_uIndex)
 {
-
+	if (a_uIndex >= m_uEntityCount)
+	{
+		a_uIndex = m_uEntityCount - 1;
+	}
+	m_entityList[a_uIndex]->SetModelMatrix(a_m4ToWorld);
 }
 //The big 3
 MyEntityManager::MyEntityManager(){Init();}
@@ -64,33 +90,49 @@ MyEntityManager::~MyEntityManager(){Release();};
 // other methods
 void Simplex::MyEntityManager::Update(void)
 {
-
+	for (int i = 0; i < m_uEntityCount - 1; i++)
+	{
+		for (int j = 1; j < m_uEntityCount; j++)
+		{
+			m_entityList[i]->IsColliding(m_entityList[j]);
+		}
+	}
 }
 void Simplex::MyEntityManager::AddEntity(String a_sFileName, String a_sUniqueID)
 {
-
+	m_entityList.push_back(new MyEntity(a_sFileName, a_sUniqueID));
+	++m_uEntityCount;
 }
 void Simplex::MyEntityManager::RemoveEntity(uint a_uIndex)
 {
-
+	std::swap(m_entityList[a_uIndex], m_entityList[m_uEntityCount - 1]);
+	m_entityList.pop_back();
+	--m_uEntityCount;
 }
 void Simplex::MyEntityManager::RemoveEntity(String a_sUniqueID)
 {
-
+	RemoveEntity(GetEntityIndex(a_sUniqueID));
 }
 String Simplex::MyEntityManager::GetUniqueID(uint a_uIndex)
 {
-	return "";
+	return m_entityList[a_uIndex]->GetUniqueID();
 }
 MyEntity* Simplex::MyEntityManager::GetEntity(uint a_uIndex)
 {
-	return nullptr;
+	return m_entityList[a_uIndex];
 }
 void Simplex::MyEntityManager::AddEntityToRenderList(uint a_uIndex, bool a_bRigidBody)
 {
-
+	if (a_uIndex >= m_uEntityCount)
+	{
+		for (int i = 0; i < m_uEntityCount; i++)
+		{
+			m_entityList[i]->AddToRenderList(a_bRigidBody);
+		}
+	}
+	else m_entityList[a_uIndex]->AddToRenderList(a_bRigidBody);
 }
 void Simplex::MyEntityManager::AddEntityToRenderList(String a_sUniqueID, bool a_bRigidBody)
 {
-
+	MyEntity::GetEntity(a_sUniqueID)->AddToRenderList(a_bRigidBody);
 }
---
+++ E08 - EntityManager/README.txt
@@ -0,0 +1,3 @@
+There's one issue where models show they're colliding by default until they
+move. The creeper will be fixed once I hit an arrow key, but the other models
+remain red.
